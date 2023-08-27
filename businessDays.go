883c691ed0726f0a8b2ca1127ada6b77474a0038
Parthav99 <57442455+Parthav99@users.noreply.github.com>
2023-08-27 15:22:17 +0530

    Add files via upload
    New business code

--- businessDays.go
+++ businessDays.go
@@ -1,11 +1,12 @@
 package main
+
 //bug: negative business days
 import (
-	"assignmentDependencies/inputs"
 	"bufio"
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,12 +18,13 @@ func main() {
 	//feeds input into the map
 	for {
 		fmt.Print("Enter a holiday(YYYY-MM-DD) or stop: ")
-		holidayStr := inputs.FetchInput()
+		holidayStr := fetchInput()
 
 		//break condition
 		if holidayStr == "stop" {
 			inputDate, businessDays := fetchAndValidateInput(inputReader)
-			relativeDate := calculateRelativeDate(inputDate, businessDays, holidayList)
+			relativeDate := calculateEndDate(inputDate, businessDays, holidayList)
+			// relativeDate := calculateRelativeDate(inputDate, businessDays, holidayList)
 			fmt.Println("Relative Date is", relativeDate.Format("2006-01-02"))
 			break
 		}
@@ -37,13 +39,12 @@ func main() {
 	}
 }
 
-//fetches and validates two input
+// fetches and validates two input
 func fetchAndValidateInput(inputReader *bufio.Reader) (time.Time, int64) {
 	fmt.Print("Enter Date(YYYY-MM-DD): ")
-	inputDateStr1 := inputs.FetchInput()
+	inputDateStr1 := fetchInput()
 	fmt.Print("Enter business days: ")
-	daysStr := inputs.FetchInput()
-
+	daysStr := fetchInput()
 	inputDate, errDate1 := time.Parse("2006-01-02", inputDateStr1)
 	businessDays, errDays := strconv.ParseInt(daysStr, 10, 64)
 
@@ -56,30 +57,86 @@ func fetchAndValidateInput(inputReader *bufio.Reader) (time.Time, int64) {
 }
 
 // Calculates relative date by adding/subtracting business days from a given date
-func calculateRelativeDate(inputDate time.Time, businessDays int64, holidayList map[time.Time]bool) time.Time {
-	var startDate time.Time
-	var addDay int
+// func calculateRelativeDate(inputDate time.Time, businessDays int64, holidayList map[time.Time]bool) time.Time {
+// 	var startDate time.Time
+// 	var addDay int
+// 	iterations := 0
+// 	//increments or decrements based on business days
+// 	if businessDays < 0 {
+// 		addDay = -1
+// 		businessDays = -businessDays
+// 	} else if businessDays != 0 {
+// 		addDay = 1
+// 	} else {
+// 		fmt.Println("No business days entered!")
+// 		os.Exit(1)
+// 	}
 
-	//increments or decrements based on business days
-	if businessDays < 0 {
-		addDay = -1
-		businessDays = -businessDays
-	} else if businessDays != 0 {
-		addDay = 1
-	} else {
-		fmt.Println("No business days entered!")
-		os.Exit(1)
-	}
+// 	for startDate = inputDate; businessDays >= 0; startDate = startDate.AddDate(0, 0, addDay) {
+// 		iterations++
+// 		if !holidayList[startDate] && startDate.Weekday() != time.Saturday && startDate.Weekday() != time.Sunday {
+// 			businessDays--
+// 		}
+// 	}
 
-	for startDate = inputDate; businessDays >= 0; startDate = startDate.AddDate(0, 0, addDay) {
-		if !holidayList[startDate] && startDate.Weekday() != time.Saturday && startDate.Weekday() != time.Sunday {
-			businessDays--
+// 	startDate = startDate.AddDate(0, 0, -addDay)
+// 	fmt.Println(iterations)
+// 	return startDate
+// }
+
+func calculateEndDate(inputDate time.Time, businessDays int64, holidayList map[time.Time]bool) time.Time {
+	counter := 0
+	currentDate := inputDate
+	businessDaysEntered := businessDays
+	addDay := 1
+
+	fullWeeks := businessDays / 5
+	remainingDays := businessDays % 5
+	weekendsInFullWeeks := fullWeeks * 2
+
+	if remainingDays > 0 {
+		for i := 0; i < int(remainingDays); i++ {
+			counter = counter + 1
+			if !holidayList[currentDate] && currentDate.Weekday() != time.Saturday && currentDate.Weekday() != time.Sunday {
+			} else {
+				fmt.Println("entered")
+				weekendsInFullWeeks++
+			}
+			currentDate = currentDate.AddDate(0, 0, 1)
 		}
+		fmt.Println("First loop iteration", counter)
 	}
 
-	startDate = startDate.AddDate(0, 0, -addDay)
+	// fmt.Println("Current Date:", currentDate)
+	if inputDate.Weekday() == time.Sunday {
+		weekendsInFullWeeks--
+	}
+	fmt.Println(weekendsInFullWeeks)
+	totalWeekends := weekendsInFullWeeks
+	totalDaysToAdd := businessDays + totalWeekends
+	endDate := inputDate.AddDate(0, 0, int(totalDaysToAdd))
 
-	return startDate
+	if businessDaysEntered < 0 {
+		addDay = -1
+	}
+
+	counter2 := 0
+	for holidayList[endDate] || endDate.Weekday() == time.Saturday || endDate.Weekday() == time.Sunday {
+		counter2++
+		endDate = endDate.AddDate(0, 0, addDay)
+		fmt.Println(endDate)
+	}
+	fmt.Println("Second loop iteration", counter2)
+
+	return endDate
+}
+
+// fetches input
+func fetchInput() string {
+	inputReader := bufio.NewReader(os.Stdin)
+	line, _ := inputReader.ReadString('\n')
+	line = strings.TrimSpace(line)
+	return line
 }
 
 //2023-08-25
